4bc0931f73dc36fef4d494e1724d8664098fd25b
Brian DeAngelis <b.d.deangelis@gmail.com>
2015-01-06 18:42:39 -0500

    WIP version.  Runs to completion.  Need to understand minFunc and if I need to explicitly define batch sizes for training.

--- multilayer_supervised/supervised_dnn_cost.m
+++ multilayer_supervised/supervised_dnn_cost.m
@@ -16,19 +16,15 @@ numHidden = numel(ei.layer_sizes) - 1;
 hAct = cell(numHidden+1, 1); % cell to store activations at each level
 gradStack = cell(numHidden+1, 1); % cell to store the gradients of each level
 
-keyboard;
+% Define the number of examples in the current batch
+m = size(data,2);
+
+% keyboard;
 
 
 %% forward prop
 %%% YOUR CODE HERE %%%
 % Forward propagation's purpose is to give a hypothesis value for each example for comparison to ground truth
-
-% % % Define the batch size
-% % batchSize = 1000;
-% % start = 1; % starting example
-
-% % Initialize the first layer of activations
-% hAct{1} = data;
 
 % Loop through each of the layers
 for ii = 1:size(stack,1)
@@ -37,69 +33,142 @@ for ii = 1:size(stack,1)
         
         % Expand the bias unit to be the appropriate number of examples
         bias = repmat(stack{ii}.b, 1, size(data,2));
-            
+        
         % Calculate the activations for each neuron
-        hAct{ii} = sigmoid(stack{ii}.W * data + bias);
+        if strcmp(ei.activation_fun, 'logistic')
+            hAct{ii} = sigmoid(stack{ii}.W * data + bias);
+        elseif strcmp(ei.activation_fun, 'tanh')
+%             hAct{ii} = ...
+        elseif strcmp(ei.activation_fun, 'rectLin')
+%             hAct{ii} = ...
+        end
         
     else
         
         % Expand the bias unit to be the appropriate number of examples
-        bias = repmat(stack{ii}.b, 1, size(hAct{ii-1},2));
+        bias = repmat(stack{ii}.b, 1, size(hAct{ii-1},2));  
         
         % Calculate the activations for each neuron
-        hAct{ii} = sigmoid(stack{ii}.W * hAct{ii-1} + bias); 
-    
+        if strcmp(ei.activation_fun, 'logistic')
+            hAct{ii} = sigmoid(stack{ii}.W * hAct{ii-1} + bias);
+        elseif strcmp(ei.activation_fun, 'tanh')
+%             hAct{ii} = ...
+        elseif strcmp(ei.activation_fun, 'rectLin')
+%             hAct{ii} = ...
+        end
+
     end
-%     % Given the input, calculate the activation for each neuron
-%     hAct{ii+1} = sigmoid(z(ii+1));
-    
-%     % Alternative tanh
-%     a{ii+1} = ...
-%     % Alternative rectified linear
-%     a{ii+1} = ...
 
 end
 
-keyboard;
+% keyboard;
 
 %% return here if only predictions desired.
+
 if po
   cost = -1; ceCost = -1; wCost = -1; numCorrect = -1;
-  grad = [];  
+  grad = [];
+  
+  % NOTE: need to return the predicted probabilities from the forward run
+  pred_prob = hAct{end}; % Test if this is correct later
+  
   return;
 end;
 
 %% compute cost
 %%% YOUR CODE HERE %%%
 
-% Generate a groundTruth table using the labels data
- % Isolate the hypothesized probabilities corresponding to the correct labels
-  I=sub2ind(size(hAct{end}), labels', 1:size(hAct{end},2));
-  labelH = hAct{end}(I);
+  % Get the groundTruth associated with the labeled data
+  groundTruth = full(sparse(labels, 1:size(data,2), 1));
   
-  % Generate a truth table associated with where the labels in y are correct
-  truthTable = zeros(size(hAct{end}));
-  truthTable(I) = 1;
+  % Normalize all hypothesized probabilities for each example by dividing by the sum of probabilities for each example
+  normH = bsxfun(@rdivide, hAct{end}, sum(hAct{end},1));
   
-%% compute gradients using backpropagation
-%%% YOUR CODE HERE %%%
+  % Calculate the cross-entropy cost function
+  ceCost = (-1/m)*sum(sum(groundTruth .* log(normH))); % same as f in the softmax example NOTE: We are looking at the average cost/ example in batch
 
-% gradStack =
-
+%   keyboard;
+  
+  %% compute gradients using backpropagation
+  %%% YOUR CODE HERE %%%
+  
+  % Allocate an empty cell for storage
+  
+  % Loop through each of the layers starting with the last layer to calculate the deltaErr terms for each of the associated edges
+  for kk = size(stack,1):-1:1
+      
+      % CALCULATE THE ERROR TERMS
+      % If you are at the output layer do something different
+      if kk == size(stack,1)
+          
+          % Formula will be different depending on the chosen non-linearity
+          if strcmp(ei.activation_fun, 'logistic')
+              
+              % Make the assignment for the error term
+              deltaErr{kk} = groundTruth-hAct{kk}.* hAct{kk}.*(1-hAct{kk});
+              
+          end
+          % Condition for all hidden layers
+      else
+          
+          % Formula for delta terms are different depending on the non-linearity selected
+          if strcmp(ei.activation_fun, 'logistic')
+              
+              % Make the assignment for the error term
+              deltaErr{kk} = stack{kk+1}.W' * deltaErr{kk+1} .* hAct{kk} .*(1-hAct{kk});
+          end
+      end
+      
+      % CALCULATE THE GRADIENTS
+      % Weights for the input layer --> first hidden layer use the input data as activations
+      if kk == 1
+          
+          % Compute the associated gradients for W
+          gradStack{kk}.W = (1/m) .* deltaErr{kk} * data';
+          
+      % Weights for all other layer-to-layer connections use the calculated activations
+      else
+          
+          % Compute the associated gradients for W
+          gradStack{kk}.W = (1/m) .* deltaErr{kk} * hAct{kk-1}';
+          
+      end
+      
+      % Compute the associated gradients for b
+      gradStack{kk}.b = (1/m) .* sum(deltaErr{kk},2);
+      
+  end
 %% compute weight penalty cost and gradient for non-bias terms
 %%% YOUR CODE HERE %%%
 
-% weight norm cost
-% wCost = 
+% Compute the gradient for non-bias terms (gradStack.b)
+
+
+% Initialize a variable for the layer weights
+totalW = 0;
+
+% Extract the number of layers with weights
+for jj = 1:size(stack,1)
+    
+    % Get the sum of the layer's weights
+    layerW = sum(sum((stack{jj}.W).^2)); 
+    
+    % Add this to the total
+    totalW = totalW + layerW;
+    
+end
+
+% Use the learning rate stored in ei.lambda
+wCost = ei.lambda/2 * totalW;  % Essentially, this is the sum of the squared weights for all weights in the network
 
 % Sum together the components of the cost
-% cost = ceCost + wCost;
-
-
-
+cost = ceCost + wCost;
 
 %% reshape gradients into vector
 [grad] = stack2params(gradStack);
+
+% keyboard;
+
 end
 
 
